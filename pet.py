f489dd85ae45c17271561ac9cac33cabd049c818
Emily Ochoa <eochoa156@gmail.com>
2024-07-02 00:56:45 -0700

    assignment 5 finished

---
+++ pet.py
@@ -0,0 +1,13 @@
+from flask import ( Blueprint, render_template ) 
+
+bp = Blueprint('pet', __name__, url_prefix="/pets")
+
+@bp.route('/')
+def index(): 
+    return render_template('index.html', pets=pets)
+
+import json 
+
+pets = json.load(open('pets.json'))
+print(pets)
+
